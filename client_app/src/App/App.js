260d3e115e5d6dd55eb42def3209ae769575c447
Mauro Doneda <mauro.doneda@gmail.com>
2020-09-04 17:42:50 +0100

    added the first Component - NavBar with styled components

--- client_app/src/App/App.js
+++ client_app/src/App/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import Welcome from "./Welcome";
+import styled, {css} from 'styled-components';
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
+import { AppProvider } from "./AppProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My Investments</h1>
-    </div>
+const MyButton = styled.div`
+    color: green;
+      
+    ${props => props.primary && css`
+        color: palevioletred;
+      `}
+`
+
+function App(){
+ 
+    return (
+      <div>
+      <AppLayout>
+        <AppProvider>
+          <AppBar/>
+          <Welcome/>
+        </AppProvider>
+      </AppLayout>
+      <MyButton>Hello</MyButton>
+      <MyButton primary>Hello</MyButton>
+      </div>
   );
 }
+
 
 export default App;
